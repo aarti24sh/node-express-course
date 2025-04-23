0d62bb14d55611374dddbb8dc38681b2931881b4
Aarti Shankar <kiera.rt24@gmail.com>
2025-04-22 20:42:21 -0700

    week 6 assignment

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,29 +1,28 @@
-
-
 const express = require('express')
 const app = express()
-const tasks=require('./routes/tasks')
-const connectDB= require('./db/connect')
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 require('dotenv').config()
-
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 //middleware
-
+app.use(express.static('./public'))
 app.use(express.json());
 
 //routes
-app.get('/hello',(req,res)=>{
-    res.send('Task Manager App');
-})
-app.use('/api/v1/tasks',tasks);
 
-const port=3000;
+app.use('/api/v1/tasks',tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000
 //start server
-const start= async()=>{
+const start= async() => {
 try {
- await connectDB(process.env.MONGO_URI)
- app.listen(port,console.log(`Server is running on port ${port}`))
-} catch(error){
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port,console.log(`Server is running on port ${port}`))
+  } catch (error) {
   console.log(error);
-}
+  }
 }
 start()
